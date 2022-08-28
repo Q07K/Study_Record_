c5e178d233723fcbf738c8dfca4f914fe85c547f
Q07K <kgh0730@gmail.com>
2022-08-29 07:21:11 +0900

    Study

--- CodingTest/Programmers/test.py
+++ CodingTest/Programmers/test.py
@@ -9,3 +9,20 @@ print(f'{num:#={size_}}')
 
 print(f'{.86:.2%}')
 
+num1 = 1
+num2 = 1.1
+print(f'')
+
+
+n, m = map(int, "3 16".split())
+
+sieve = [0, 0] + [1]*(m-1)
+print(sieve)
+for i in range(2, m+1):
+    if sieve[i]:
+        for j in range(i+i, m+1, i):
+            sieve[j] = 0
+        if i >=n:
+            print(i)
+            
+            
